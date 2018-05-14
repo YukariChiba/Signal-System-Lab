19e2a036c265c424c2b2257408b24677fa58d3b3
SteveCharlesYang <Charles@aic.ac.cn>
2018-05-14 22:57:31 +0800

    要提交的变更： 	修改：     Lab5/Part3.m 	新文件：   Lab5/images/Part2_1.jpg 	新文件：   Lab5/images/Part2_2.jpg 	新文件：   Lab5/images/Part2_3.jpg 	新文件：   Lab5/images/Part3_1.jpg 	新文件：   Lab5/images/Part3_2.jpg 	新文件：   Lab5/images/Part3_3.jpg

--- Lab5/Part3.m
+++ Lab5/Part3.m
@@ -9,29 +9,34 @@ nx2 = 0:length(x2)-1;
 x3 = [ones(1,8) zeros(1,24)];
 nx3 = 0:length(x3)-1;
 N = 64;
-for n = 0:N-1
-    r = rem(n,length(x1));
-    x11(n+1) = x1(r+1);
+for type = 1:3
+    for n = 0:N-1
+        eval(['r = rem(n,length(x',int2str(type),'));'])
+        eval(['x',int2str(type),int2str(type),'(n+1) = x',int2str(type),'(r+1);'])
+    end
+    eval(['subplot(31',int2str(type),');'])
+    eval(['plot(x',int2str(type),int2str(type),');xlabel("n");ylabel("x',int2str(type),'");'])
 end
-subplot(311);plot(x11);
-for n = 0:N-1
-    r = rem(n,length(x2));
-    x22(n+1) = x2(r+1);
-end
-subplot(312);plot(x22);
-for n = 0:N-1
-    r = rem(n,length(x3));
-    x33(n+1) = x3(r+1);
-end
-subplot(313);plot(x33);
 %% (e)
-a1 = (1/N)*fft(x11);
-a2 = (1/N)*fft(x22);
-a3 = (1/N)*fft(x33);
 figure;
-subplot(311);stem(abs(a1));
-subplot(312);stem(abs(a2));
-subplot(313);stem(abs(a3));
-%% (f)
-
-%% (g)
+for type = 1:3
+    eval(['a',int2str(type),' = (1/N)*fft(x',int2str(type),int2str(type),');'])
+    eval(['subplot(31',int2str(type),');stem(abs(a',int2str(type),'));xlabel("n");ylabel("y',int2str(type),'");'])
+end    
+%% (f)(h)
+N = N/2;
+n = 0:N-1;
+xx = (1/N)*fft(x33(1:N));
+figure;
+index=1;
+for type = [2 8 12 16]
+    eval(['subplot(22', int2str(index),');'])
+    eval(['x3_', int2str(type), '=0;'])
+    for counter=2:type+1
+        eval(['x3_', int2str(type), '= x3_', int2str(type), '+ xx(counter) * exp(1i*(counter-1)*(2*pi/32)*n);'])
+        eval(['x3_', int2str(type), '= x3_', int2str(type), '+ conj(xx(counter)) * exp(1i*(1-counter)*(2*pi/32)*n);'])
+    end
+    eval(['x3_', int2str(type), '= x3_', int2str(type), '+ xx(1);'])
+    eval(['stem(x3_', int2str(type),');xlabel("n");ylabel("x3_{',int2str(type),'}");'])
+    index = index+1;
+end
